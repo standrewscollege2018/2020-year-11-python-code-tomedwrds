679ba2b33975660246da4363b7946c57bd5a67ab
ted4895 <60488797+ted4895@users.noreply.github.com>
2020-02-14 10:40:17 +1300

    finished tic tac toe

--- Tic Tac Toe.py
+++ Tic Tac Toe.py
@@ -18,9 +18,9 @@ def draw_board():
 
 #Define the game
 
-game = [["1", "2", "3"],
-        ["4", "5", "6"],
-        ["7", "8", "9"]]
+game = [[" ", " ", " "],
+        [" ", " ", " "],
+        [" ", " ", " "]]
 
 playing_game = True
 
@@ -31,22 +31,62 @@ while playing_game :
     
     draw_board()
     
-    #Take a coodirnate
-    move = input("Please enter the codinate of you move x,y: ")
+    making_move = True
+    while making_move :
+        try:
+            #Take a coodirnate
+            move = input("Please enter the codinate of you move x,y: ")
+            
+            #Remove any whitespace
+            move = move.strip()
+            
+            move_y = int(move[0])
+            move_x = int(move[2])
+            
+            #Check if place is taken
+            if game[move_x][move_y] == " ":
+                
+                game[move_x][move_y] = token
+                making_move = False
+                
+            else:
+                print("ERROR: THe square is taken")
+        
+        
+        except:
+            print("ERROR: Please enter a valid value")
+            
+        
+        
+      
+    #Check for win
     
-    #Remove any whitespace
-    move = move.strip()
     
-    move_y = int(move[0])
-    move_x = int(move[1])
+    row_complete = (game[move_x][0] == game[move_x][1] == game[move_x][2])
+    column_complete = (game[0][move_y] == game[1][move_y] == game[2][move_y])
+    diagonal_rl_complete = (game[0][0] == game[1][1] == game[2][2])
+    diagonal_lr_complete = (game[2][0] == game[1][1] == game[0][2])
     
-    game[move_x][move_y] = token
+    if row_complete or column_complete or diagonal_rl_complete or diagonal_lr_complete:
+        
+        if token == "x":
+            print("Player one wins")
+        else:
+            print("Player two wins")
+        
+        
+        cont = input("Press any key to continue")
+        
+        
     
+        
     if token == "x":
         token = "o"
     elif token == "o":
         token = "x"
         
+        
+  
         
 
 
